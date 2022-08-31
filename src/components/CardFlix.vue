3b8befc9e8eb7a2436ade80482ec05e9b4ec562d
ErikBorgogno <erikborgo.20@gmail.com>
2022-08-31 10:15:51 +0200

    Milestone 1(remove css)

--- src/components/CardFlix.vue
+++ src/components/CardFlix.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="card">
-        <h2>{{info.title}}</h2>
-        <h3>{{info.original_title}}</h3>
-        <div><span>{{info.original_language}}</span></div>
-        <div class="vote">{{info.vote_average}}</div>    
-    </div>
+    <ul>
+        <li>{{info.title}} -
+            {{info.original_title}} -
+            {{info.original_language}} -
+            {{info.vote_average}}
+        </li>    
+    </ul>
 </template>
 
 <script>
@@ -17,28 +18,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    .card{
-        width: calc(100% / 5 - 70px);
-        margin: 0 35px;
-        height: 350px;
-        padding: 10px 0;
-        border: 1px solid white;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-        text-align: center;
-        h2{
-            padding: 10px 0;
-            color: gainsboro;
-        }
-        span{
-            font-size: 1.2rem;
-            color: greenyellow;
-        }
-        .vote{
-            color: beige;
-        }
-
-    }
+    
 </style>
--- src/components/MainFlix.vue
+++ src/components/MainFlix.vue
@@ -21,16 +21,5 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    main{
-        width: 100%;
-        height: 100%;
-        background-color: rgb(48, 48, 122);
-        .container{
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: space-evenly;
-            flex-wrap: wrap;
-        }
-    }
+    
 </style>
